bd2f07e3a65c4f59d3817bd1f3bdcf6d8d07bae6
Franz Vezuli <franz.vezuli@flashtalking.com>
2023-11-22 20:08:42 -0500

    minimum mvp.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { eventcalendar, toast, util } from '@mobiscroll/javascript';
+  import { onMount } from 'svelte';
+
+  onMount(() => {
+    const inst = eventcalendar('#demo-desktop-week-view', {
+      theme: 'ios',
+      themeVariant: 'light',
+      clickToCreate: true,
+      dragToCreate: true,
+      dragToMove: true,
+      dragToResize: true,
+      eventDelete: true,
+      view: {
+        schedule: { type: 'week' },
+      },
+      onEventClick: function (event) {
+        toast({
+          message: event.event.title,
+        });
+      },
+    });
+
+    util.http.getJson(
+        'https://trial.mobiscroll.com/events/?vers=5',
+        function (events) {
+          inst.setEvents(events);
+        },
+        'jsonp',
+    );
+  });
+</script>
+
+<div id="demo-desktop-week-view"></div>
